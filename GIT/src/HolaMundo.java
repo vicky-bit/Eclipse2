bfee8e804ca57fc60fa5474da7440dc0c094adf4
profesor <Vicky>
2023-03-23 07:29:47 +0100

    Rama 1 añade comentario

--- GIT/src/HolaMundo.java
+++ GIT/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Soy rama 1");
 	}
 
 }
